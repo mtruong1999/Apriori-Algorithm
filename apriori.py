84f2134337e5e5953d6b5234110d3c7adc8bc1bb
Michael <36463000+mtruong1999@users.noreply.github.com>
2020-09-11 18:25:21 -0600

    First commit

---
+++ apriori.py
@@ -0,0 +1,19 @@
+'''
+CS 521 HW2
+This program implements the APRIORI algorithm to find the set of closed patterns 
+from the dataset found in data/.
+
+Author: Michael Truong
+'''
+import os
+import time
+import numpy as np
+import scipy.io as sio
+
+DATA_FILE_LOCATION = os.path.join('data', 'dataAPRIORI.mat')
+
+
+if __name__ == "__main__":
+    # Load data
+    data = sio.loadmat(DATA_FILE_LOCATION)['data']
+
