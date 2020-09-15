c08905b32ed375b05f5babc4e15d31a932cec934
Michael <36463000+mtruong1999@users.noreply.github.com>
2020-09-14 22:06:42 -0600

    implement rest of functions
    Implement count_candidates, min_support_candidates, and apriori. Apriori currently returns a list of all frequent itemsets rather than only the closed itemsets

--- apriori.py
+++ apriori.py
@@ -9,13 +9,15 @@ import os
 import time
 import itertools
 import scipy.io as sio
+import numpy as np
 
 DATA_FILE_LOCATION = os.path.join('data', 'dataAPRIORI.mat')
 MIN_SUP = 0.15
 
-class CandidateList(list):
+class CandidateItem(list):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.count = 0
 
 def union(list_1, list_2):
     '''
@@ -43,7 +45,7 @@ def generate_candidates(L_k, k):
             candidates.add(union_)
     
     # Convert candidates from set to list type
-    candidates = [list(candidate) for candidate in candidates]
+    candidates = [CandidateItem(candidate) for candidate in candidates]
 
     # Prune
     # TODO: Verify this works for all edge cases
@@ -64,20 +66,45 @@ def count_candidates(C, transaction):
     Returns the count of the number of appearences of each
     candidates in `C` found in `transaction`
     '''
-    pass
+    for candidate in C:
+        if all(transaction[elem] == 1 for elem in candidate):
+            candidate.count+=1
 
-def min_support_candidates(candidate_counts, min_support):
+def min_support_candidates(candidates, min_support):
     '''
     Returns the candidates that meets the min_support requirements
     '''
-    pass
+    frequent_candidates = []
+    for candidate in candidates:
+        if candidate.count >= min_support:
+            frequent_candidates.append(list(candidate))
+    return frequent_candidates
 
-def apriori_(data):
+def apriori_(data, frequency):
     '''
     Runs APRIORI algorithm on data and returns all of the closed
     itemsets and their frequencies
     '''
-    pass
+    k = 1
+    absolute_freq = int(frequency * data.shape[0])
+    single_itemset_count = np.sum(data, axis = 0)
+    bool_freq_array = single_itemset_count >= absolute_freq
+
+    # We let item j be the column index and thus get the column indices
+    # whose column sum satisfies min frequency requirement
+    L = [item[0] for item in enumerate(bool_freq_array) if item[1]]
+    union_L = []
+    while len(L) != 0:
+        union_L.append(L)
+        C_k1 = generate_candidates(L, k)
+        for transaction in data:
+            count_candidates(C_k1, transaction)
+        L = min_support_candidates(C_k1, absolute_freq)
+        k += 1
+    
+    return union_L
+
+
 
 if __name__ == "__main__":
     # Load data
